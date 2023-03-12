aa6942eb5d9c5fdb5855c6874ea18e8023a7b006
Roman Varanita <romanvaranita@gmail.com>
2023-03-12 20:00:56 +0100

    add atribut

--- src/Pet.java
+++ src/Pet.java
@@ -1,3 +1,18 @@
 public class Pet {
 
+  public String kind;
+
+  public String nickname;
+
+  public String birthdate;
+
+  public int weight;
+
+  public Pet(String kind, String nickname, String birthdate, int weight);{
+    this.kind = kind;
+    this.nickname = nickname;
+    this.birthdate = birthdate;
+    this.weight = weight;
+  }
+
 }
